af6161a965186a3903164e05df2d14fc4c33262b
lahir <lahir@NITRO5>
2024-03-28 01:03:14 +0530

    add

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -9,9 +9,7 @@ import Footer from './Footer';
 import Nav from './Navbar';
 
 const Specials = () => {
-    // Retrieve specials data from local storage
-    const storedSpecialsData = localStorage.getItem('specials');
-    const specials = storedSpecialsData ? JSON.parse(storedSpecialsData) : [
+    const data = [
         {
             "product": "Organic Apples",
             "price": "$2.99 per pound"
@@ -24,19 +22,21 @@ const Specials = () => {
             "product": "Free-range Eggs",
             "price": "$3.99 per dozen"
         }
-    ]
-    ;
+    ];
+    localStorage.setItem('specials', JSON.stringify(data));
+    const storedSpecialsData = localStorage.getItem('specials');
+    const specials = storedSpecialsData ? JSON.parse(storedSpecialsData) : [];
 
     return (
         <div>
             <Nav />
 
-            <Container maxW="600px" mt="50px" p="20px" borderWidth="1px" borderRadius="5px" boxShadow="0 2px 5px rgba(0, 0, 0, 0.1)" style={{ margin: '0 auto', marginBottom:'150px', marginTop: '150px', height: "400px" }}>
+            <Container maxW="600px" mt="50px" p="20px" borderWidth="1px" borderRadius="5px" boxShadow="0 2px 5px rgba(0, 0, 0, 0.1)" style={{ margin: '0 auto', marginBottom:'150px', marginTop: '150px', height: "auto" }}>
                 <Heading textAlign="center" fontSize="28px" mb="20px">Specials of the Week</Heading>
                 {specials.length > 0 ? (
                     specials.map((special, index) => (
-                        <Box key={index} mb="20px">
-                            <Text fontSize="18px" fontWeight="bold">Product: {special.product}</Text>
+                        <Box key={index} mb="20px" p="20px" borderRadius="5px" bg="#f8f8f8" boxShadow="0 2px 5px rgba(0, 0, 0, 0.1)">
+                            <Text fontSize="18px" fontWeight="bold" mb="10px">Product: {special.product}</Text>
                             <Text fontSize="16px">Price: {special.price}</Text>
                         </Box>
                     ))
